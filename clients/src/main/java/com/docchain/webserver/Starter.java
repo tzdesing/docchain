a6772e3aa1c6b787ed12a1b3a2e49a82d75b0582
Pereira Sales <joes@gft.com>
2021-10-29 12:53:27 -0300

    mapper bean

--- clients/src/main/java/com/docchain/webserver/Starter.java
+++ clients/src/main/java/com/docchain/webserver/Starter.java
@@ -1,8 +1,12 @@
 package com.docchain.webserver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import static org.springframework.boot.WebApplicationType.SERVLET;
 
@@ -19,5 +23,13 @@ public class Starter {
         app.setBannerMode(Banner.Mode.OFF);
         app.setWebApplicationType(SERVLET);
         app.run(args);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure( SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
+        return converter;
     }
 }
